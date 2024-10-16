696d090a5795ecdd639f1885a8ba26adda4bf9d1
YarikMex <yarikfuentes@hotmail.com>
2024-10-16 11:32:38 -0600

    env

--- resources/views/inicio2.blade.php
+++ resources/views/inicio2.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CATALGOS</title>
+    <title>Inicio 2</title>
     {{-- vite sirve el boostrap --}}
     @vite(['resources/js/app.js'])
     
