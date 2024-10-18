64b78d82e1f977efa249b85ab6d8eedd48bb0fcd
YarikMex <yarikfuentes@hotmail.com>
2024-10-17 21:09:00 -0600

    diseno

--- resources/views/menu2.blade.php
+++ resources/views/menu2.blade.php
@@ -1,51 +1,39 @@
  <!-- Nav tabs -->
-
- <ul
- class="nav nav-tabs"
- id="navId"
- role="tablist"
->
- <li class="nav-item">
-     <a
-         href="#tab1Id"
-         class="nav-link active"
-         data-bs-toggle="tab"
-         aria-current="page"
-         >Bienvenidos</a
-     >
- </li>
-
-    {{-- CATALOGO --}}
-    <li class="nav-item" role="presentation">
-        <a href="{{route('catalogo')}}" class="nav-link" 
-            >Catalogos</a>
+ <ul class="nav nav-tabs" id="navId" role="tablist">
+    <li class="nav-item">
+        <a href="#tab1Id" class="nav-link active" data-bs-toggle="tab" aria-current="page">Bienvenidos</a>
     </li>
 
-    {{-- HORARIOS --}}
-    <li class="nav-item" role="presentation">
-        <a href="{{route('horarios')}}" class="nav-link" 
-            >Horarios</a>
+    {{-- CATALOGO con dropdown --}}
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="catalogoDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Catálogos
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="catalogoDropdown">
+            <li><a class="dropdown-item" href="{{ route('alumnos.index') }}">Alumnos</a></li>
+            <li><a class="dropdown-item" href="{{ route('plazas.index') }}">Plazas</a></li>
+            <li><a class="dropdown-item" href="{{ route('puestos.index') }}">Puestos</a></li>
+        </ul>
     </li>
 
-    {{-- PROYECTOS IND. --}}
+    {{-- Otras opciones del menú --}}
     <li class="nav-item" role="presentation">
-        <a href="{{route('proyectosIndividuales')}}" class="nav-link" 
-            >Proyectos individuales </a>
+        <a href="{{ route('horarios') }}" class="nav-link">Horarios</a>
     </li>
 
-    {{-- INSTRUMENTACION --}}
     <li class="nav-item" role="presentation">
-        <a href="{{route('instrumentacion')}}" class="nav-link" 
-            >Instrumentacion</a>
+        <a href="{{ route('proyectosIndividuales') }}" class="nav-link">Proyectos individuales</a>
     </li>
 
-    {{-- TUTORIAS --}}
     <li class="nav-item" role="presentation">
-        <a href="{{route('tutorias')}}" class="nav-link" 
-            >Tutorias </a>
+        <a href="{{ route('instrumentacion') }}" class="nav-link">Instrumentación</a>
     </li>
 
-    {{-- PERIODO - SELECT --}}
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('tutorias') }}" class="nav-link">Tutorías</a>
+    </li>
+
+    {{-- Periodos --}}
     <li class="nav-item">
         <select class="form-select" aria-label="Seleccionar periodo">
             <option selected disabled>Periodo</option>
@@ -54,31 +42,19 @@
             <option value="ene-jun-25">Ene-Jun 25</option>
         </select>
     </li>
-    
 
- {{-- guest estas como invitado --}}
- {{-- si no esta autentificado --}}
- @guest 
+    @guest
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('login') }}" class="nav-link">Logeo</a>
+    </li>
+    @endguest
 
- <li class="nav-item" role="presentation">
-     <a href="{{route('login')}}" class="nav-link" >Logeo</a>
- </li>
-@endguest
-
-
- {{-- auth si esta autentificado --}}
-@auth
-<li class="nav-item" role="presentation">
-    <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        Log Out
-    </a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
-</li>
-
-
-@endauth    
+    @auth
+    <li class="nav-item" role="presentation">
+        <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    </li>
+    @endauth    
 </ul>
-
-  
