9bbe6d2a81193e386a8d2e109161254260c2216b
YarikMex <yarikfuentes@hotmail.com>
2024-10-17 21:36:43 -0600

    CambioPlaza

--- resources/views/plazas/tablahtml.blade.php
+++ resources/views/plazas/tablahtml.blade.php
@@ -2,7 +2,33 @@
     <p>{{$mensaje}}</p>
 @endisset
 
-<a href="{{route('plazas.create')}}" class="btn button btn-dark" style="margin-bottom: 15px;" role="button">Insertar</a>
+<!-- Formulario para insertar nueva Plaza -->
+<h2>Insertar Plaza</h2>
+<form action="{{ route('plazas.store') }}" method="POST">
+    @csrf
+
+    <!-- Nombre de la Plaza -->
+    <div class="row mb-3">
+        <label for="nombrePlaza" class="col-sm-2 col-form-label">Nombre de la Plaza</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="nombrePlaza" name="nombrePlaza" required value="{{ old('nombrePlaza') }}">
+          @error("nombrePlaza")
+          <p class="text-danger">Error en: {{ $message }}</p>
+          @enderror
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-sm-10 offset-sm-2">
+          <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
+    </div>
+</form>
+
+<!-- Botón para insertar nueva plaza (eliminado, ya no es necesario) -->
+{{-- <a href="{{route('plazas.create')}}" class="btn button btn-dark" style="margin-bottom: 15px;" role="button">Insertar</a> --}}
+
+<!-- Tabla de plazas -->
 <div class="table-md">
     <table class="table table-primary">
         <thead>
