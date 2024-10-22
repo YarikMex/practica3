514011902a24d98f06956e2c8cc17a4448c1a06c
YarikMex <yarikfuentes@hotmail.com>
2024-10-21 22:32:35 -0600

    depto

--- database/migrations/2024_08_08_192852_create_carreras_table.php
+++ database/migrations/2024_08_08_192852_create_carreras_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('carreras', function (Blueprint $table) {
             $table->id();
-            $table->string("idcarrera",15)->unique();
             $table->string("nombrecarrera",100)->unique();
             $table->string("nombremediano",100)->unique();
             $table->string("nombrecorto",100)->unique();
