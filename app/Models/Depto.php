12f008e7017bc47b30f06a80f8aa3a6988514f19
YarikMex <yarikfuentes@hotmail.com>
2024-10-21 19:41:41 -0600

    CambioAlumnos

--- app/Models/Depto.php
+++ app/Models/Depto.php
@@ -10,6 +10,12 @@ class Depto extends Model
 {
     use HasFactory;
 
+    // Especificar la clave primaria
+    protected $primaryKey = 'idDepto'; 
+
+    // Si no usas 'id' como clave primaria, asegurarte que no es auto-incremental (opcional)
+    public $incrementing = true; // Solo si tu idDepto es auto-incremental, mantenlo en true
+
     // Campos que se pueden llenar masivamente
     protected $fillable = [
         'nombredepto',
