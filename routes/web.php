bf57eee6da651989f20cfd2ad7e5ad4326470ab0
YarikMex <yarikfuentes@hotmail.com>
2024-10-22 22:14:37 -0600

    CambioPlaza

--- routes/web.php
+++ routes/web.php
@@ -203,6 +203,7 @@ Route::post('/materias/{materia}', [MateriaController::class, 'update'])->name('
 Route::post('/materias/{materia}/destroy', [MateriaController::class, 'destroy'])->name('materias.destroy');
 Route::get('/materias/{materia}', [MateriaController::class, 'show'])->name('materias.show');
 
+// Rutas de Periodos en CRUD
 Route::get('/periodos', [PeriodoController::class, 'index'])->name('periodos.index');
 Route::get('/periodos/create', [PeriodoController::class, 'create'])->name('periodos.create');
 Route::post('/periodos', [PeriodoController::class, 'store'])->name('periodos.store');
