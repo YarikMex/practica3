48f91bebc1e5868b1a857097225dd6f512356e5c
YarikMex <yarikfuentes@hotmail.com>
2024-10-22 19:12:37 -0600

    VIVAJESUCRISTO

--- resources/views/reticulas/index.blade.php
+++ resources/views/reticulas/index.blade.php
@@ -4,11 +4,3 @@
     @include('reticulas.tablahtml')
 @endsection
 
-@section('contenido2')
-<h1>Retículas</h1>
-<a href="{{ route('reticulas.create') }}" class="btn btn-primary mb-3">Insertar Retícula</a>
-
-<div class="table-responsive">
-    @include('reticulas.tablahtml')
-</div>
-@endsection
