c4c5f584094590cff265335a3889bf86f11d4067
YarikMex <yarikfuentes@hotmail.com>
2024-10-22 20:16:04 -0600

    CambioDeptos

--- database/seeders/AlumnoSeeder.php
+++ database/seeders/AlumnoSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carrera;
 use App\Models\Alumno;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class AlumnoSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Alumno::factory(10)->create();
+        // Crear varias carreras primero
+        Carrera::factory(5)->create();
+
+        // Luego, crear los alumnos
+        Alumno::factory(5)->create();
     }
 }
