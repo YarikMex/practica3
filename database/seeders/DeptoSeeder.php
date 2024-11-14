342357694d7252612da213a301909c0c35e37173
YarikMex <yarikfuentes@hotmail.com>
2024-11-13 20:45:42 -0600

    Descripción de los cambios realizados

--- database/seeders/DeptoSeeder.php
+++ database/seeders/DeptoSeeder.php
@@ -12,6 +12,23 @@ class DeptoSeeder extends Seeder
      */
     public function run(): void
     {
-        Depto::factory(10)->create(); // Crear 10 departamentos de ejemplo
+        // Lista de departamentos con datos específicos
+        $deptos = [
+            ['nombredepto' => 'Ing. en Sistemas Computacionales', 'nombremediano' => 'Sistemas Comp.', 'nombrecorto' => 'ISC'],
+            ['nombredepto' => 'Ing. en Electrónica', 'nombremediano' => 'Electrónica', 'nombrecorto' => 'IE'],
+            ['nombredepto' => 'Ing. Mecánica', 'nombremediano' => 'Mecánica', 'nombrecorto' => 'IM'],
+            ['nombredepto' => 'Ing. Industrial', 'nombremediano' => 'Industrial', 'nombrecorto' => 'II'],
+            ['nombredepto' => 'Ing. en Gestión Empresarial', 'nombremediano' => 'Gestión Emp.', 'nombrecorto' => 'IGE'],
+            ['nombredepto' => 'Contador Público', 'nombremediano' => 'Contaduría', 'nombrecorto' => 'CP'],
+            ['nombredepto' => 'Ing. Mecatrónica', 'nombremediano' => 'Mecatrónica', 'nombrecorto' => 'IMT'],
+            ['nombredepto' => 'Dirección', 'nombremediano' => 'Dirección', 'nombrecorto' => 'DIR'],
+            ['nombredepto' => 'Subdirección', 'nombremediano' => 'Subdirección', 'nombrecorto' => 'SUB'],
+            ['nombredepto' => 'Ciencias Básicas', 'nombremediano' => 'Ciencias Bás.', 'nombrecorto' => 'CB'],
+        ];
+
+        // Crear o encontrar cada departamento para evitar duplicados
+        foreach ($deptos as $deptoData) {
+            Depto::firstOrCreate($deptoData);
+        }
     }
 }
